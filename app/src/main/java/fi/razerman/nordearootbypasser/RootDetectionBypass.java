dded66e91032c5a2add071dd4134a323bba9b802
Razer2015 <razer138@gmail.com>
2017-06-27 12:50:16 +0300

    Added support for Nordea Codes v1.5.1

--- app/src/main/java/fi/razerman/nordearootbypasser/RootDetectionBypass.java
+++ app/src/main/java/fi/razerman/nordearootbypasser/RootDetectionBypass.java
@@ -4,51 +4,95 @@ package fi.razerman.nordearootbypasser;
  * Created by Razerman on 1.10.2016.
  */
 
-import android.util.Log;
+import android.content.Context;
+import android.content.pm.PackageManager;
 
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import static de.robv.android.xposed.XposedBridge.log;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-import static de.robv.android.xposed.XC_MethodReplacement.returnConstant;
-import static de.robv.android.xposed.XposedHelpers.setBooleanField;
 
-import java.util.ArrayList;
+import static de.robv.android.xposed.XC_MethodReplacement.returnConstant;
+import static de.robv.android.xposed.XposedBridge.log;
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
+import android.util.Log;
 import java.util.List;
 
 public class RootDetectionBypass implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("com.nordea.mobiletoken")){
+            final String APP_PACKAGE = "com.nordea.mobiletoken";
             Log.d("nordearootbypasser", "Nordea Codes app detected, starting to bypass root detection!");
+
+            int versionCode = 0;
             try {
-                findAndHookMethod("o.ˊ", lpparam.classLoader, "ʻ", returnConstant(false));                                  // Check 1
-                findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", returnConstant(false));                                  // Check 2
-                findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", String.class, returnConstant(false));                    // Check 3
-                findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", String.class, String.class, returnConstant(false));      // Check 4
-                findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", new byte[0].getClass(), returnConstant(false));          // Check 5
-                findAndHookMethod("o.ˊ", lpparam.classLoader, "ˋ", returnConstant(false));                                  // Check 6
-                findAndHookMethod("o.ˊ", lpparam.classLoader, "ˋ", String.class, String.class, returnConstant(false));      // Check 7
+                final Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+                final Context context = (Context) callMethod(activityThread, "getSystemContext");
+                versionCode = context.getPackageManager().getPackageInfo(APP_PACKAGE, 0).versionCode;
+                Log.d("nordearootbypasser", "Version: " + versionCode);
 
-                Log.d("nordearootbypasser", "Bypassed Nordea Codes root detection!");
-            } catch (Throwable t) {
-                log(t);
+            } catch (PackageManager.NameNotFoundException e) {
+                //Handle exception
+                Log.d("nordearootbypasser", "Couldn't retrieve version.");
+            }
 
+            if((versionCode / 100) >= 151){
                 try{
-                    Log.d("nordearootbypasser", "Nordea Codes root bypass 1 failed, trying another one!");
+                    Log.d("nordearootbypasser", "Starting to bypass Nordea Codes version 1.5.1 or higher.");
+
+                    findAndHookMethod("util.m.d", lpparam.classLoader, "b", new byte[0].getClass(), List.class, returnConstant(new byte[] { 1, 0, 0 })); // Hook detection O_O
+
+                    findAndHookMethod("util.m.d", lpparam.classLoader, "c", new byte[0].getClass(), returnConstant(new byte[] { 1, 0, 0 })); // Root detection
+
+                    Log.d("nordearootbypasser", "Nordea Codes root bypass succeeded!");
+                } catch (Throwable err) {
+                    log(err);
+                }
+            }
+            else if((versionCode / 100) == 150){
+                try{
+                    Log.d("nordearootbypasser", "Starting to bypass Nordea Codes version 1.5.0");
 
                     findAndHookMethod("util.m.e", lpparam.classLoader, "d", new byte[0].getClass(), List.class, returnConstant(new byte[] { 1, 0, 0 })); // Hook detection O_O
 
                     findAndHookMethod("util.m.e", lpparam.classLoader, "d", new byte[0].getClass(), returnConstant(new byte[] { 1, 0, 0 })); // Root detection
 
-                    Log.d("nordearootbypasser", "Nordea Codes root bypass 2 succeeded!");
+                    Log.d("nordearootbypasser", "Nordea Codes root bypass succeeded!");
                 } catch (Throwable err) {
                     log(err);
+                }
+            }
+            else{
+                try {
+                    Log.d("nordearootbypasser", "Starting to bypass Nordea Codes version " + versionCode);
+
+                    findAndHookMethod("o.ˊ", lpparam.classLoader, "ʻ", returnConstant(false));                                  // Check 1
+                    findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", returnConstant(false));                                  // Check 2
+                    findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", String.class, returnConstant(false));                    // Check 3
+                    findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", String.class, String.class, returnConstant(false));      // Check 4
+                    findAndHookMethod("o.ˊ", lpparam.classLoader, "ˊ", new byte[0].getClass(), returnConstant(false));          // Check 5
+                    findAndHookMethod("o.ˊ", lpparam.classLoader, "ˋ", returnConstant(false));                                  // Check 6
+                    findAndHookMethod("o.ˊ", lpparam.classLoader, "ˋ", String.class, String.class, returnConstant(false));      // Check 7
+
+                    Log.d("nordearootbypasser", "Bypassed Nordea Codes root detection!");
+                } catch (Throwable t) {
+                    log(t);
                 }
             }
         }
 
         if (lpparam.packageName.equals("fi.nordea.mep.npay")){
+            try {
+                Log.d("nordearootbypasser", "Trying to bypass NFC check!");
+
+                findAndHookMethod("clf", lpparam.classLoader, "d", returnConstant(true));
+
+            } catch (Throwable t) {
+                log(t);
+            }
+
             Log.d("nordearootbypasser", "Nordea Pay app detected, starting to bypass root detection!");
             try {
                 // Checking order is: c, d, e, f, g, b
